b6566a2c7fec165d5583ea34aa30d3401e0ae33b
Paulo Dutra <paulodutra.info@gmail.com>
2023-10-18 10:11:52 -0300

    feat: change the methods name to reflect the bussiness rules

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -2,6 +2,7 @@ class Customer {
     _id: string;
     _name: string;
     _address: string;
+    _active: boolean = true;
 
     constructor(id: string, name: string, address: string) {
         this._id = id;
@@ -9,27 +10,17 @@ class Customer {
         this._address = address;
     }
 
-    get id(): string {
-        return this._id
-    }
-
-    get name(): string {
-        return this._name;
-    }
-
-    get address(): string {
-        return this.address;
-    }
-
-    set id(id: string) {
-        this.id = id;
-    }
-
-    set name(name: string) {
+    changeName(name: string) {
         this._name = name;
-    } 
+    }
 
-    set address(address: string) {
-        this._address = address;
-    } 
+    activate() {
+        this._active = true;
+    }
+
+    deactivate() {
+        this._active = false;
+    }
+
+
 }
