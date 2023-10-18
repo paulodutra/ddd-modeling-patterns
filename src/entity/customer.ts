24077128ba527031333416c575289f039154d4af
Paulo Dutra <paulodutra.info@gmail.com>
2023-10-18 10:27:39 -0300

    feat: add the autovalidate inside of entity

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,20 +1,34 @@
+const EMPTY = 0;
 class Customer {
     _id: string;
     _name: string;
-    _address: string;
-    _active: boolean = true;
+    _address: string = '';
+    _active: boolean = false;
 
-    constructor(id: string, name: string, address: string) {
+    constructor(id: string, name: string) {
         this._id = id;
         this._name = name;
-        this._address = address;
+        this.validate();
+    }
+
+    validate() {
+        if (this._name.length === EMPTY) {
+            throw new Error("Name is required");
+        }
+        if (this._id.length === EMPTY) {
+            throw new Error("Id is required");
+        }
     }
 
     changeName(name: string) {
         this._name = name;
+        this.validate();
     }
 
     activate() {
+        if (this._address.length === EMPTY) {
+            throw new Error("Address is mandatory to activate a customer");
+        }
         this._active = true;
     }
 
