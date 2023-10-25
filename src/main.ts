3c9acd201bacaec4809fa147d6092fe4fe4308f9
Paulo Dutra <paulodutra.info@gmail.com>
2023-10-24 23:11:08 -0300

    feat: created new entities and the example of aggregate

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import Address from "./entity/Address";
+import Customer from "./entity/customer";
+import Order from "./entity/order";
+import OrderItem from "./entity/orderItem";
+
+//aggregate 1, relation with object
+const customer = new Customer('123', 'Paulo');
+const address = new Address('rua', 2, '0000000', 'Sao Paulo');
+customer.Address = address;
+customer.activate();
+
+//aggregate 2, relation using ID to reference de aggregate 1 e object beetween Order and OrderItem
+const item1 = new OrderItem('1','Item', 10);
+const item2 = new OrderItem('1','Item 2', 15);
+const order = new Order('1', '123', [item1, item2]);
+
+
