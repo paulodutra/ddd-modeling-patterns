c06e9fc9ded6c4ac0ddf3a2031491e86ef0b840c
Paulo Dutra <paulodutra.info@gmail.com>
2023-10-27 17:58:34 -0300

    feat: implementing self validation

--- src/entity/order.ts
+++ src/entity/order.ts
@@ -2,14 +2,32 @@ import OrderItem from './orderItem';
 
 export default class Order {
 
-    _id: string;
-    _customerId: string;
-    _items: OrderItem[];
+    private _id: string;
+    private _customerId: string;
+    private _items: OrderItem[];
+    private _total: number;
 
     constructor(id: string, customerId: string, items: OrderItem[]) {
         this._id = id;
         this._customerId = customerId;
         this._items = items;
+        this._total = this.total();
+
+        this.validate();
+    }
+
+    validate(): boolean {
+        if (this._id.length === 0) {
+            throw new Error("Id is required");
+        }
+        if (this._customerId.length === 0) {
+            throw new Error("customerId is required");
+        }
+        if (this._items.length === 0) {
+            throw new Error("items quantity must be greater than 0");
+        }
+
+        return true;
     }
 
     total(): number {
