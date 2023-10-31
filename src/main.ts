bc10e7e1582fa655cf6aaa7a411737fbd0a2dc97
Paulo Dutra <paulodutra.info@gmail.com>
2023-10-31 19:13:46 -0300

    test: configurating test command to check the types before run the tests

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+//@ts-ignore
 import Address from "./entity/Address";
 import Customer from "./entity/customer";
 import Order from "./entity/order";
@@ -10,8 +11,8 @@ customer.Address = address;
 customer.activate();
 
 //aggregate 2, relation using ID to reference de aggregate 1 e object beetween Order and OrderItem
-const item1 = new OrderItem('1','Item', 10);
-const item2 = new OrderItem('1','Item 2', 15);
+const item1 = new OrderItem('1','Item', 100, "p1", 1);
+const item2 = new OrderItem('1','Item 2', 100, "p2", 2);
 const order = new Order('1', '123', [item1, item2]);
 console.log(order);
 
