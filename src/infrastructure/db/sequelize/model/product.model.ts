5f428931965fce2dfbf34c67608482fbf8120c4e
Paulo Dutra <paulodutra.info@gmail.com>
2023-12-30 17:37:27 -0300

    chore: started config sequelize

---
+++ src/infrastructure/db/sequelize/model/product.model.ts
@@ -0,0 +1,19 @@
+import { Column, PrimaryKey, Table } from 'sequelize-typescript';
+import Model from 'sequelize/types/model';
+
+@Table({
+    tableName: 'products',
+    timestamps: false
+})
+export default class ProductModel extends Model {
+
+    @PrimaryKey
+    @Column
+    declare id: string;
+
+    @Column({ allowNull: false })
+    declare name: string;
+
+    @Column({ allowNull: false })
+    declare price: string;
+}
